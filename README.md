d61189e7b92a4fef3fa02e258e29ede761c9a7f1
DJReflexive <89596189+DJReflexive@users.noreply.github.com>
2024-12-02 16:24:48 -0700

    Not a Submission. Added memoization.

--- README.md
+++ README.md
@@ -54,9 +54,9 @@ My Analysis...
 
 # Sources
 
-- KobeLimon21's Repository on GitHub (https://github.com/COSC3020/tsp-held-karp-KobeLimon21/blob/main/code.js) - Borrowed some ideas to help my tsp_hk() method to function properly. Specifically for having every city become the start city every iteration. I did not borrow from it for anything else, and checking whether a new best minimum value had been found.
-- Google's Gemini AI - For the quick description of how to remove a specific element with .splice()
+- KobeLimon21's Repository on GitHub (https://github.com/COSC3020/tsp-held-karp-KobeLimon21/blob/main/code.js) - Borrowed some ideas to help my tsp_hk() method to function properly. Specifically for having every city become the start city every iteration, and checking whether a new best minimum value had been found. I did not borrow from it for anything else.
 - https://www.youtube.com/watch?v=jUYAJ72m8P0&t=379s - For how to run the held-kelp algorithm by hand and giving insight to how this works.
+- https://www.geeksforgeeks.org/how-to-create-dictionary-and-add-key-value-pairs-dynamically/# - For documentation on use object literal notation as a dictionary.
 
 # Plagiarism Acknowledgement
 
--- code.js
+++ code.js
@@ -9,10 +9,13 @@ function tsp_hk(distance_matrix) {
 
     // Make Every City the Starting Node
     for (let i = 0; i < size; i++) {
+        let dictionaryCache = {};
         let cities = [];
+
+        // Fill array with the all cities
         for (let j = 0; j < size; j++) { cities.push(j); }
 
-        let currentMin = heldKarp(i, cities, distance_matrix);
+        let currentMin = heldKarp(i, cities, distance_matrix, dictionaryCache);
 
         // Replaces current min with new min
         if (currentMin < bestMin) { bestMin = currentMin; }
@@ -23,37 +26,45 @@ function tsp_hk(distance_matrix) {
 
 
 // Based on the psuedocode in the README
-function heldKarp(city, cities, matrix) {
+function heldKarp(city, cities, matrix, cache) {
     let size = cities.length;
+
+    // Check if it exists already in cache
+    let key = JSON.stringify([city, cities]);
+    if (cache[key] != undefined) { return cache[key]; }
     
+
     // Base Case when there are two cities
     if (size == 2) {
         for (let i = 0; i < size; i++) {
             if (cities[i] != city) {
-                return matrix[city][cities[i]];
+                cache[key] = matrix[city][cities[i]];
+                return cache[key];
             }
         }
     } else { 
         let minDistance = Infinity;
 
         for (let i = 0; i < size; i++) {
-            if (i == city) continue; // Skips current city
+            if (cities[i] == city) { continue; } // Skips current city
 
             let newCities = [];
 
             // Removes current city from cities list
             for (let j = 0; j < size; j++) {
-                if (j == city) continue; // Skips current city
+                if (cities[j] == city) { continue; } // Skips current city
 
                 newCities.push(cities[j]);
             }
             
-            let distance = matrix[city][cities[i]] + heldKarp(cities[i], newCities, matrix);
+            let distance = matrix[city][cities[i]] 
+                            + heldKarp(cities[i], newCities, matrix, cache);
 
             // Find the min
             if (minDistance > distance) { minDistance = distance; }
         }
 
+        cache[key] = minDistance;
         return minDistance;
     }
 }
